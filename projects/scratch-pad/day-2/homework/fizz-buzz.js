a2c73bbd8401cc4f886910ab9849786de41cf2e0
Ubuntu <ubuntu@ip-172-31-77-218.ec2.internal>
2021-06-22 20:06:30 +0000

    third website update

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -13,7 +13,21 @@
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
-
+    var result = [];
+    
+    for (var i = 1; i <= 100; i++) {
+        if (i % 5 === 0 && i % 3 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else {
+            result.push(i);
+        }
+    }
+    
+    return result;
 
     // YOUR CODE GOES ABOVE HERE //
 }
