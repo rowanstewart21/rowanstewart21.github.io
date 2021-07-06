7e8859535c3efc10633b94fbc30cd87f28204d8c
Ubuntu <ubuntu@ip-172-31-77-218.ec2.internal>
2021-07-06 18:50:11 +0000

    width adjustments

--- studies/datatypes.js
+++ studies/datatypes.js
@@ -1,24 +1,91 @@
-// Datatypes
-// Number
-// String
-// Boolean
-// Array
-// Object
-// Function
-// undefined
-// null
-// NaN
-// Infinity and -Infinity (Google them if you don't know!)
 // What is the difference between primitive/simple and complex data types?
-// Primitive values are passed to a function BY COPY, complex values are BY REFERENCE. What does that mean, and how are they different?
+// Primitive values are passed to a function BY COPY, complex values are BY REFERENCE. 
+// What does that mean, and how are they different?
 
-/* There are a bunch of different data types that can be stored in 
-   JavaScript - each of the datatypes stores a different type of 
-   information and can be used in different ways.
-*/
+/* 
+ * DATA TYPES :
+ *
+ * 0. there are a bunch of different datatypes that can be stored in javascript - each of
+ * the datatypes stores a different type of information and can be used in different 
+ * ways. the different datatypes are broken into two larger categories: simple and 
+ * complex data. knowing the datatype of a variable is important because it affects 
+ * the operators that can be used on the value or variable. 
+ *
+ * 1. there are 8 different datatypes: number, string, boolean, array, object, function, 
+ * undefined, null, NaN, and infinity / -infinity. 
+ *
+ * 2. there are two different subcategories of datatypes: simple / primitive and complex; 
+ * these datatypes differ in what they contain and how they are referenced
+ * 
+ * 3. some datatypes get passed by COPY while others get passed by REFERENCE - this 
+ * depends on whether the value is a simple or primitive type and affects whether the
+ * copy is updated when the variable is.
+ */ 
 
-// 1. the first datatype is number - these are integers and floating decimal numbeers
+// 1. datatypes //
 
-var myNumber = 421;
+var myNumber = 21;
 
-      // 
+/* numbers are any numerical data, either integer or floating decimal. we can use 
+ * arithmetic operators to manipulate numerical data the same way we would in math 
+ * (for example, myNumber + 21 would return 42). we can also use comparison and logical
+ * operators with numbers, to compare and evaluate if statements.
+ */
+
+var myString = 'hello, world!';
+/* strings are character data and contain letters and words. strings are contained in 
+ * quotations marks. quotation marks around any other datatype will convert the data to
+ * a string (for example, 42 is a number, but '42' is a string). we can use arithmetic, 
+ * comparison, and logical operators on strings.
+ */
+
+var myBoolean = true;
+/* boolean values are either true or false - boolean values are most frequently used 
+ * when working with if statements, where the program completes one action if a statement
+ * is true and a different action if the statement is false
+ */
+
+var myUndefined;
+/* undefined means that the variable has not been initialized or assigned yet - the 
+ * variable name may exist, but it has not been assigned a value.
+ */
+ 
+var myNull = null;
+/* null values mean that the value doesn't exist or has been nullified by the programmer.
+ * this means that the metaphorical "bucket" of our variable is empty. 
+ */
+
+var myArray = ['rowan', 24, true];
+/* arrays are like lists of other values - they can contain any of the simple datatypes
+ * within their list. arrays are created using [square brackets] and commas between 
+ * values. we can use several built-in methods with arrays, such as push, unshift, pop,
+ * etc. we use these with dot notation (for example, to push something to the end of 
+ * myArray, we would use myArray.push('math teacher')). our new array would be 
+ * ['rowan', 24, true, 'math teacher']
+ *
+ * we access elements in an array using bracket notation. for example, to access the
+ * first element in the array myArray, we would use myArray[0]. we can access the last
+ * element of an array using the same notation, but replacing 0 with array.length - 1. 
+ * this lets us access the last index without hardcoding (myArray[array.length - 1])
+ */
+
+var myObject = {name: 'rowan', age: 24, collegeGrad: true, job: 'math teacher'};
+/* objects are similar to arrays in that they hold a collection of values, but objects
+ * are sets of key-value pairs, meaning that they have an attribute or key that is 
+ * assigned a value. for example, in myObject, the attribute or key name is assigned 
+ * a value 'rowan'. we could create another object, 'abby' with the same keys to store
+ * the same information about Abby : abby = {name: 'abby', age: 24, collegeGrad: true, 
+ * job: 'TU advancement program coordinator'}
+ *
+ * objects are created using {curly brackets} and commas between key:value pairs. we 
+ * can use dot notation to add new key:value pairs to objects (for example, to add 
+ * favoriteAnimal to myObject, we would use myObject.favoriteAnimal = 'elephant' - 
+ * our new object would read {name: 'rowan', age: 24, collegeGrad: true, job: 'math 
+ * teacher', favoriteAnimal = 'elephant'};
+ */
+ 
+var myFunction = function(x){
+      return x + 21
+   }
+/* 
+ */
--- studies/variables.js
+++ studies/variables.js
@@ -1,19 +1,21 @@
-console.log(myFaveFood); // prints > 'undefined'
+console.log(myFaveFood); // prints > 'undefined' but doesn't show an error 
 console.log(myFaveColor); // shows an error - let variables are not hoisted
 console.log(myFaveFruit); // shows an error - const variables are not hoisted
 
 /*
  * VARIABLES :
  *
- * 0. to hold things in memory during the life-cycle of a program, we can use variables.  variables
- * are named identifiers that can point to values of a particular type, like a number, string,
- * boolean, array, object or another data-type.  variables are called so because once created, we
- * can CHANGE the value (and type of value) to which they point.
+ * 0. to hold things in memory during the life-cycle of a program, we can use variables. 
+ * variables are named identifiers that can point to values of a particular type, like 
+ * a number, string, boolean, array, object or another data-type.  variables are called
+ * so because once created, we can CHANGE the value (and type of value) to which they 
+ * point.
  *
- * 1. to create a variable we use the keyword, var, followed by a name (id or alias) for our
- * variable.
+ * 1. to create a variable we use the keyword, var, followed by a name (id or alias)
+ * for our variable.
  *
- * 2. there are 2 phases of using variables: declaration and initialization (or assignment).
+ * 2. there are 2 phases of using variables: declaration and initialization (or 
+ * assignment).
  */
 
 // 1. declaration //
@@ -21,9 +23,9 @@ console.log(myFaveFruit); // shows an error - const variables are not hoisted
 var myName;
 
 /*
- * at the declaration phase, the variable myName is undefined because we have NOT initialized
- * it to anything. right now, the variable exists, but it isn't assigned to anything - calling it just 
- * prints undefined
+ * at the declaration phase, the variable myName is undefined because we have NOT 
+ * initialized it to anything. right now, the variable exists, but it isn't assigned
+ * to anything - calling it just prints undefined
  */
 
 console.log(myName); // prints > undefined
@@ -38,7 +40,7 @@ console.log(myName); // prints > 'rowan'
 myName = 'bob';
 console.log(myName); // prints > 'bob'
 
-// NOTE: we can assign and re-assign anything to a variable - we cannot do this with constants //
+// NOTE: we can assign and re-assign anything to a var - we can't do this with const //
 var myVariable = 1;
 
 myVariable = true;
@@ -47,9 +49,9 @@ console.log (myVariable); // prints > true
 myVariable = "someString";
 console.log(myVariable); // prints > 'someString'
 
-/* the value of myVariable changes - if we console.log myVariable after line 44, it will print true, 
- * but if we console.log it after line 47, it will print "someString" - this is because the value of
- * myVariable has been changed to "someString"
+/* the value of myVariable changes - if we console.log myVariable after line 44, it will
+ * print true, but if we console.log it after line 47, it will print "someString" - this
+ * is because the value of myVariable has been changed to "someString"
  */
 
 // 4. let and const //
@@ -60,13 +62,13 @@ myFavoriteNum = 22; // this is not allowed - let and var can be reassigned
 const myBirthday = '04/21/97';
 myBirthday = '04/22/97'; // this is not allowed - const cannot be reassigned
 
-/* let and const also declare variables, but have slightly different properties than the keyword var.
- * let and const are both block-scoped variables, which means that they only exist within the scope 
- * where they're created. 
+/* let and const also declare variables, but have slightly different properties than the
+ * keyword var. let and const are both block-scoped variables, which means that they only
+ * exist within the scope where they're created. 
  *
- * the big difference between let and const is that let can be reassigned, while const cannot - const
- * is short for constant, meaning that the value does not change. let can be reassigned (but is still
- * constrained to the block in which it was created)
+ * the big difference between let and const is that let can be reassigned, while const
+ * cannot - const is short for constant, meaning that the value does not change. let can
+ * be reassigned (but is still constrained to the block in which it was created)
  */
  
 if (myName === 'rowan') {
@@ -75,31 +77,34 @@ if (myName === 'rowan') {
     const myLastName = 'not stewart';
 }
 
-/* console.log(myLastName); shows an error because myLastName is not defined outside of the if 
- * statement in which it was created; we could use it within the if statement, but it cannot be
- * called outside of that block
+/* console.log(myLastName); shows an error because myLastName is not defined outside of 
+ * the if statement in which it was created; we could use it within the if statement, but
+ * it cannot be called outside of that block
  */
  
 // 5. hoisting //
 
 var myFaveFood = 'pasta';
 
-/* variables can be declared anywhere within a program and are 'hoisted' to the top before the program
- * runs. when a computer runs through a program, it runs through twice - once to search for all variable
- * and function declarations (which get "hoisted" or pulled up to the top) and then again to execute
- * the actual code. this is sort of like putting together a bookcase - you read the instructions once
- * to figure out what tools you need (variables and functions) and then read them again to actually 
- * build the bookcase. 
+/* variables can be declared anywhere within a program and are 'hoisted' to the top 
+ * before the program runs. when a computer runs through a program, it runs through 
+ * twice - once to search for all variable and function declarations (which get "hoisted"
+ * or pulled up to the top) and then again to execute the actual code. this is sort of 
+ * like putting together a bookcase - you read the instructions once to figure out what 
+ * tools you need (variables and functions) and then read them again to actually build 
+ * the bookcase. 
  *
- * we could call the variable myFaveFood at the top of the program (see line 1) - since the variable is
- * hoisted, the variable has already been declared before the start of the program. however, the 
- * variable hasn't been INITIALIZED or ASSIGNED yet, so the console.log will print undefined. the
- * assignment is on line 85 - anywhere it's called before there will be undefined (but the var will exist)
+ * we could call the variable myFaveFood at the top of the program (see line 1) - since 
+ * the variable is hoisted, the variable has already been declared before the start of 
+ * the program. however, the  variable hasn't been INITIALIZED or ASSIGNED yet, so the 
+ * console.log will print undefined. the assignment is on line 85 - anywhere it's called 
+ * before there will be undefined (but the var will exist)
  */
  
 let myFaveColor = 'purple';
 const myFaveFruit = 'strawberry'
 
-/* let and const are not hoisted - these variables myFaveColor and myFaveFruit do not exist until
- * lines 100 and 101 where they are declared. calling them earlier in the program will show an error
+/* let and const are not hoisted - these variables myFaveColor and myFaveFruit do not 
+ * exist until lines 100 and 101 where they are declared. calling them earlier in the
+ * program will show an error
  */
